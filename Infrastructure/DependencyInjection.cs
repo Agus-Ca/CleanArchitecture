ca54af2d5a29063451e387f94334d9a02d235dc1
Agustín Casado <agu.e.casado@gmail.com>
2022-07-16 21:56:49 -0300

    injectamos las bases de datos

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+using Infrastructure.Persistence.Dapper;
+using Infrastructure.Persistence.EntityFramework;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddEFDatabase(configuration);
+        services.AddDapperDatabase(configuration);
+        
+        return services;
+    }
+}
