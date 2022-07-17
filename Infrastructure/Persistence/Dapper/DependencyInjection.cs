139fcc631e0c5d4f33c04e30dd1001dd46fbd7b1
Agustín Casado <agu.e.casado@gmail.com>
2022-07-16 21:56:34 -0300

    Agregamos la base de datos de dapper

---
+++ Infrastructure/Persistence/Dapper/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Persistence.Dapper
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDapperDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DapperDatabase");
+
+            services.AddTransient<IDbConnection>((sp) => new SqlConnection(connectionString));
+
+            return services;
+        }
+    }
+}
