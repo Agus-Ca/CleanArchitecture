6df0eb6c129dbe86b63e4415f7464d4dda6736c3
Agustín Casado <agu.e.casado@gmail.com>
2022-07-16 23:22:23 -0300

    agregamos e injectamos MediatR

--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -1,7 +1,12 @@
+using Application;
+using MediatR;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+builder.Services.AddMediatR(typeof(ApplicationMediatREntryPoint).Assembly);
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
