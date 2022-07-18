142905a3b6080e92b61bae3bf9a7665bb7e43d61
Agustín Casado <agu.e.casado@gmail.com>
2022-07-18 13:00:48 -0300

    pantalla home (comidas)

--- WebUI/ClientApp/src/app/comidas/comidas.module.ts
+++ WebUI/ClientApp/src/app/comidas/comidas.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { ComidasRoutingModule } from './comidas-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { ComidaComponent } from './pages/comida/comida.component';
@@ -21,6 +22,7 @@ import { ListadoComponent } from './pages/listado/listado.component';
   imports: [
     CommonModule,
     FlexLayoutModule,
+    MaterialModule,
     ComidasRoutingModule
   ]
 })
